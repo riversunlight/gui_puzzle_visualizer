a71ebdb3d0483b389b56b21fe0e6736022bb2962
riversunlight <aabbee107@icloud.com>
2021-12-16 20:49:07 +0900

    fix view picture's size

--- gui_puzzle_visualizer/draw.cpp
+++ gui_puzzle_visualizer/draw.cpp
@@ -4,7 +4,7 @@
 #include "answer.h"
 
 void draw(vector<vector<DynamicTexture> > &tex, picture &pic, vector<int> &rotate) {
-    const int PSIZE = 64;
+    const int PSIZE = 680 / pic.div_y;
 	const int ax = 20, ay = 20;
 	for (int y = 0; y < pic.div_y; y++) {
 		for (int x = 0; x < pic.div_x; x++) {
